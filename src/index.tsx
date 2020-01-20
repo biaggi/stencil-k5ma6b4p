90cf3a51d999230c22688885808ad6d25923d635
Jose Manuel <biaggi@gmail.com>
2020-01-20 10:11:19 +0000

    attributes are auto reflected to the host

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,13 @@ import { h, Component, Prop } from "@stencil/core";
 class HelloComponent {
   @Prop({ reflect: true }) name: string;
   render() {
-    return <button>Hello {this.name ? this.name : "World"}</button>;
+    return (
+      <button onClick={this.click.bind(this)}>
+        Hello {this.name ? this.name : "World"}
+      </button>
+    );
+  }
+  click() {
+    this.name = "test";
   }
 }
