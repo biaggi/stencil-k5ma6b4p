e040491675b0a4e7c9a91b7b81dc5ffbea89bd44
Jose Manuel <biaggi@gmail.com>
2020-01-20 10:04:58 +0000

    watch attribute

--- src/index.stories.js
+++ src/index.stories.js
@@ -3,4 +3,13 @@ import { html } from "lit-html";
 
 export const story1 = () => html`
   <custom-element name="you"> </custom-element>
+  <button
+    @click=${() => {
+      document
+        .getElementsByTagName("custom-element")[0]
+        .setAttribute("name", "jm");
+    }}
+  >
+    test
+  </button>
 `;
